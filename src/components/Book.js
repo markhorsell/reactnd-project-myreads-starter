2ae59242123cca255afa77833648345ab535b25a
DESKTOP-HUC2SSV\markh <mark.horsell@gmail.com>
2017-08-13 18:10:14 +0100

    router and propTypes working, book list and search components added

---
+++ src/components/Book.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+import '../App.css'
+
+class Book extends React.Component {
+   
+    render() {
+         const { title, authors, cover } = this.props;
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:cover }}></div>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{authors}</div>
+            </div>
+        )
+    }
+}
+Book.propTypes = {
+       title: PropTypes.string.isRequired,
+        authors: PropTypes.string.isRequired,
+         cover: PropTypes.string.isRequired,
+        
+    }
+
+
+export default Book;
